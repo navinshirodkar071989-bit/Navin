b9c7be007bf467f79d4e9a08428c71130dd6c15f
navinshirodkar071989-bit <navinshirodkar071989@gmail.com>
2026-04-27 16:06:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,42 +2,52 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 
+# Title
 st.title("📊 Smart Stock App")
+st.subheader("📈 Live Stock Analysis")
 
-# Select stock
-stock = st.selectbox("Select Stock", ["RELIANCE.NS","TCS.NS","INFY.NS"])
+# Stock selection
+stock = st.selectbox("Select Stock", [
+    "RELIANCE.NS",
+    "TCS.NS",
+    "INFY.NS",
+    "HDFCBANK.NS"
+])
 
-# Select time
+# Time selection
 period = st.selectbox("Select Period", ["6mo", "1y", "2y"])
 
-# Get data
+# Download data
 df = yf.download(stock, period=period)
 
 # Fix column issue
 if isinstance(df.columns, pd.MultiIndex):
     df.columns = df.columns.get_level_values(0)
 
-# If no data
+# Check if data exists
 if df.empty:
     st.write("No data found")
 
 else:
-    # Moving average
+    # Moving averages
     df['MA50'] = df['Close'].rolling(50).mean()
     df['MA200'] = df['Close'].rolling(200).mean()
 
-    # Check data
+    # Check enough data
     if df['MA200'].isna().all():
         st.write("Not enough data for analysis")
 
     else:
         latest = df.iloc[-1]
 
+        # BUY/SELL signal
         if latest['MA50'] > latest['MA200']:
-            st.write("🟢 BUY Signal")
+            st.success("🟢 BUY Signal")
         else:
-            st.write("🔴 SELL Signal")
+            st.error("🔴 SELL Signal")
 
-        st.write("Price:", latest['Close'])
+        # Price box
+        st.metric("💰 Current Price", round(latest['Close'], 2))
 
+        # Chart
         st.line_chart(df[['Close','MA50','MA200']])
