57a8ab1636334677d9d7efdd66608d98afc6c017
navinshirodkar071989-bit <navinshirodkar071989@gmail.com>
2026-04-27 16:17:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ for stock in stocks:
     gain = (delta.where(delta > 0, 0)).rolling(14).mean()
     loss = (-delta.where(delta < 0, 0)).rolling(14).mean()
     rs = gain / loss
-    df['RSI'] = 100 - (100 / (1 + rs))
+    df['RSI'] = 100 - (100 / (1 + rs)
 
     # MACD
     exp1 = df['Close'].ewm(span=12, adjust=False).mean()
